ef62bff9a4546d58ff85b04ddeaad2cf57853a88
jaeHoonLee <jaehoonlee88@gmail.com>
2013-07-26 03:22:22 +0900

    socketItem added, room enter leave

--- public/javascripts/socket.js
+++ public/javascripts/socket.js
@@ -1,6 +1,6 @@
+var socket = io.connect("http://127.0.0.1:3001/") ;
 $(document).ready(function()
 {
-    var socket = io.connect("http://127.0.0.1:3001/") ;
 //    var socket = io.connect("http://jhun88.cafe24.com:3000/") ;
     socket.on('connect', function(){
         console.log("connected");
@@ -18,6 +18,20 @@ $(document).ready(function()
         console.log(data);
     });
 
-    socket.emit('signup',  {name:"JH"});
-    socket.emit('roomList',  {name:"JH"});
+//    socket.emit('signup',  {name:"JH"});
+//    socket.emit('roomList');
+//    socket.emit('roomMake', {name:"MADMAD", owner:"adbdse"});
+      socket.emit('roomEnter', {room_id:"1", id:"21025"});
+//    socket.emit('roomLeave', {id:"21025"});
 });
+
+
+function plus()
+{
+    socket.emit('roomEnter', {room_id:"1", id:"21025"});
+}
+
+function minus()
+{
+    socket.emit('roomLeave', {id:"21025"});
+}
